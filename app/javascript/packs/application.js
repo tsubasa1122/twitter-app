a657e016d2c59b1eb2c41776dff9267905c60ff2
tsubasa1122 <nakanot@open8.com>
2019-12-18 13:14:07 +0900

    [fix]bootstrapの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap';
+import '../stylesheets/application';
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import "bootstrap"
-import "../stylesheets/application"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
