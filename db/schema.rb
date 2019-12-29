2bbd03225cccec36712a79d1e5b112a9ac6c2fea
tsubasa1122 <nakanot@open8.com>
2019-12-29 14:50:00 +0900

    [add]nameとusernameカラムTo Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_105851) do
+ActiveRecord::Schema.define(version: 2019_12_29_053939) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "username", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
