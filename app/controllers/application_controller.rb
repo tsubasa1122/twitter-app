1dd656a22491d6ed1a46cadc7eae94edc1e36dd7
tsubasa1122 <nakanot@open8.com>
2019-12-29 15:16:35 +0900

    [add]strongmarameter to device_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = %i[name username email password password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: %i[email, password]
+  end
 end
