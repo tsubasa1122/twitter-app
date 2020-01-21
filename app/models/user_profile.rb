b4ba970398742000ca39d71a4b7899eba8c0ac81
tsubasa1122 <nakanot@open8.com>
2020-01-21 19:27:16 +0900

    [add]user_profile関連のvalidation

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -3,4 +3,7 @@ class UserProfile < ApplicationRecord
 
   validates :name, presence: true, length: { in: 1..50 }
   validates :account_name, presence: true, length: { in: 1..15 }, uniqueness: true
+  validates :description, length: { maximum: 160 }
+  validates :location, length: { maximum: 30 }
+  validates :website, length: { maximum: 100 }
 end
