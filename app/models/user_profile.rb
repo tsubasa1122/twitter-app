f465d6eb4c94921628e884a5fdc789daa65e7a2b
tsubasa1122 <nakanot@open8.com>
2020-01-25 11:02:20 +0900

    [fix]case_sensitive: tureにする

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -2,7 +2,7 @@ class UserProfile < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { in: 1..50 }
-  validates :account_name, presence: true, length: { in: 1..15 }, uniqueness: true
+  validates :account_name, presence: true, length: { in: 1..15 }, uniqueness: { case_sensitive: true }
   validates :description, length: { maximum: 160 }
   validates :location, length: { maximum: 30 }
   validates :website, length: { maximum: 100 }
