b6bd9c7c31f4bcf0b87cecbf43026d7a3827c18d
tsubasa1122 <nakanot@open8.com>
2020-01-25 10:19:28 +0900

    [add]profile_image to user_profile

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,9 +1,21 @@
 class UserProfile < ApplicationRecord
   belongs_to :user
+  has_one_attached :profile_image
 
   validates :name, presence: true, length: { in: 1..50 }
   validates :account_name, presence: true, length: { in: 1..15 }, uniqueness: true
   validates :description, length: { maximum: 160 }
   validates :location, length: { maximum: 30 }
   validates :website, length: { maximum: 100 }
+  validate :file_type_image
+
+  def file_type_image
+    errors.add(:profile_image, "画像データではありません。") unless image?
+  end
+
+  def image?
+    return nil unless profile_image.attached?
+
+    %w[image/jpg image/jpeg image/png image/gif].include?(profile_image.blob.content_type)
+  end
 end
