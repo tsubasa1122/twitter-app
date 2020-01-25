2d719c17317579292e94993018e86854056e5276
tsubasa1122 <nakanot@open8.com>
2020-01-25 10:17:49 +0900

    [fix]update時もset_userを行うように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: %i[show update]
   
   def show; end
 
   def update
-    @user_profile = UserProfile.find_by(user_id: params[:id])
     if @user_profile.update(user_profile_params)
       flash[:notice] = "ユーザー情報を更新しました。"
       redirect_to user_path(@user_profile.user_id)
@@ -12,13 +11,12 @@ class UsersController < ApplicationController
       flash[:alert] = "更新に失敗しました。"
       render :show
     end
-
   end
 
   private
 
   def user_profile_params
-    params.require(:user_profile).permit(:name, :account_name, :description, :location, :website)
+    params.require(:user_profile).permit(:name, :account_name, :description, :location, :website, :profile_image)
   end
 
   def set_user
