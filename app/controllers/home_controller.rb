a359063aa7d2c1be96cc3816f81dc8b2b6dc1492
tsubasa1122 <nakanot@open8.com>
2020-02-02 19:15:29 +0900

    [add]一旦tweetを投稿出来るように

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @tweet = Tweet.new(user_id: current_user.id)
   end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,19 @@
 class TweetsController < ApplicationController
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      flash[:notice] = "Tweetが投稿されました。"
+      redirect_to root_path
+    else
+      flash[:alert] = "Tweetの投稿に失敗しました。"
+      render "home/index"
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:user_id, :content)
+  end
 end
