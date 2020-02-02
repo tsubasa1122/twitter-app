feec6fd9404898e731a15284c1f9313464c17b4e
tsubasa1122 <nakanot@open8.com>
2020-02-02 18:42:50 +0900

    [add]tweetモデル作成

---
+++ db/migrate/20200202094017_create_tweets.rb
@@ -0,0 +1,9 @@
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.references :user, foreign_key: true
+      t.text :content
+      t.timestamps
+    end
+  end
+end
