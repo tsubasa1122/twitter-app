f6a10e1071888bfaa299f16883bba0a9afedab9d
tsubasa1122 <nakanot@open8.com>
2020-02-05 10:10:18 +0900

    [fix]レビュー修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,6 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = Tweet.new(tweet_params)
-    @tweet.user_id = current_user.id
     if @tweet.save
       flash[:notice] = "Tweetが投稿されました。"
       redirect_to root_path
@@ -15,6 +14,6 @@ class TweetsController < ApplicationController
   private
 
   def tweet_params
-    params.require(:tweet).permit(:content)
+    params.require(:tweet).permit(:content).merge(user_id: current_user.id)
   end
 end
