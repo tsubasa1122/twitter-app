20e3238c20f7fd7e3e6d02000ef9569c9d26e71d
tsubasa1122 <nakanot@open8.com>
2020-02-24 03:06:01 +0900

    [add]relationshipのroute作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  resources :users, only: %i[show update]
+  resources :users, only: %i[show update] do
+    resource :relationships, only: %i[create destroy]
+    get :followers, on: :member
+    get :follows, on: :member
+  end
   resources :tweets, only: %i[create]
 end
