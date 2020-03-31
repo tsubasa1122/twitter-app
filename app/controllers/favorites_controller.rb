7967d429dad5f4ca6292883c4d6dd1ca3691cbc0
tsubasa1122 <nakanot@open8.com>
2020-03-31 16:34:07 +0900

    [add]favorite作成・削除のコントローラー

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,13 @@
 class FavoritesController < ApplicationController
   def create
+    favorite = current_user.favorites.build(tweet_id: params[:tweet_id])
+    favorite.save
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    favorite = Favorite.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
+    favorite.destroy
+    redirect_back(fallback_location: root_path)
   end
 end
