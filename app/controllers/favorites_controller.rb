f838e42f84e22b6f8597f7c68b51ba4441ea5904
tsubasa1122 <nakanot@open8.com>
2020-04-02 12:01:23 +0900

    [fix]strong_parameterを使用するように

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,19 @@
 class FavoritesController < ApplicationController
   def create
-    favorite = current_user.favorites.build(tweet_id: params[:tweet_id])
+    favorite = current_user.favorites.build(tweet_id: favorite_params[:tweet_id])
     favorite.save
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    favorite = Favorite.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
+    favorite = Favorite.find_by(tweet_id: favorite_params[:tweet_id], user_id: current_user.id)
     favorite.destroy
     redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:tweet_id)
   end
 end
